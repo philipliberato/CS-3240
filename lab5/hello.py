9b37004a07d5deaecfcd6c32b00f1b0bb80407ad
Philip Liberato <philipliberato@gmail.com>
2014-09-29 14:18:16 -0400

    Added the function: greeting(msg)

--- lab5/hello.py
+++ lab5/hello.py
@@ -1,1 +1,11 @@
-print 'hello'
+"""Philip Liberato: an incredibly basic hello.py"""
+
+
+def greeting(msg):
+    print msg
+
+message = raw_input("Please input a message: ")
+greeting(message)
+
+
+
